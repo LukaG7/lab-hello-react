5b6a831bd6980a6d9cdc089076fb43aeb5297a9a
Lukas Gonzalez <lukegonz47@gmail.com>
2022-09-26 14:55:12 -0400

    done

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import ih from './images/ironhack-logo.png';
+import dash from './images/dash-dash.png';
 import icon1 from './images/icon-1.png';
 import icon2 from './images/icon-2.png';
 import icon3 from './images/icon-3.png';
@@ -11,6 +12,7 @@ function App() {
   <body>
     <div className="App">
       <img className='ih-logo' src={ih}/>
+      <img className='dash' src={dash}/>
       <div className='header'>
         <h1>Say Hello to React JS</h1>
         <p>You will learn how to use the most popular frontend library,
@@ -25,12 +27,28 @@ function App() {
       <img className='Logo-6' src={logo}/>
     </div>
 
-    <div className='icon-div'>
-      <img className='icon-img' src={icon1}/>
-      <img className='icon-img' src={icon2}/>
-      <img className='icon-img' src={icon3}/>
-      <img className='icon-img' src={icon4}/>
-    </div>
+    <section className='icon-section'>
+        <div className='icons-div'>
+          <img className='icon-img' src={icon1}/>
+          <h2>Declarative</h2>
+          <p className='img-p'>React makes it painless to create interactive UIs.</p>
+        </div>
+        <div className='icons-div'>
+          <img className='icon-img' src={icon2}/>
+          <h2>Components</h2>
+          <p className='img-p'>Build encapsulated components that manage their state.</p>      
+        </div>
+        <div className='icons-div'>
+          <img className='icon-img' src={icon3}/>
+          <h2>Single-Way</h2>
+          <p className='img-p'>A set of immutable values are passed to the component's.</p>
+        </div>
+        <div className='icons-div'>
+          <img className='icon-img' src={icon4}/>
+          <h2>JSX</h2>
+          <p className='img-p' >Statically-typed, designed to run on modern browsers.</p>
+        </div>
+      </section>
 
   </body>
   );
